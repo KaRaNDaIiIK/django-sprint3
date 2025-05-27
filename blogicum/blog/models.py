94d51ae5728fe0fdfafe035853bd8255615b2889
Yura Donchev <Donchev.yus@yandex.ru>
2025-05-27 17:37:56 +0500

    /

--- blogicum/blog/models.py
+++ blogicum/blog/models.py
@@ -94,7 +94,7 @@ class Post(PublishedModel):
     """Модель поста."""
 
     title = models.CharField(
-        max_length={MAX_TITLE_LEN},
+        max_length=MAX_TITLE_LEN,
         verbose_name='Заголовок',
         help_text=(f'Публикация, не более {MAX_TITLE_LEN} символов'),
     )
